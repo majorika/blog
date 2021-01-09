4428c3eea5dcf1480b08fceb5a72525877339885
Youngkyoung, Lee <majorika@gmail.com>
2021-01-09 15:23:19 +0900

    change build scripts

--- deploy.bat
+++ deploy.bat
@@ -4,12 +4,6 @@ rmdir /s /q docs
 
 hugo -d docs
 
-del docs\used-opensource\index.*
-del docs\what-i-made\index.*
-del docs\privacy\index.*
-
-echo "Committing changes..."
-
 git add --all .
-git commit -m "rebuilding site %DATE% %TIME%"
+git commit -m "rebuilding site"
 git push origin master
--- deploy.sh
+++ deploy.sh
@@ -1,27 +1,7 @@
 rm -fr docs
 
-# Build the project.
 hugo -d docs
 
-rm ./docs/used-opensource/index.*
-rm ./docs/what-i-made/index.*
-rm ./docs/privacy/index.*
-
-echo -e "\033[0;32mCommitting changes...\033[0m"
-
-# Add changes to git.
 git add --all .
-
-# Commit changes.
-msg="rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
-git commit -m "$msg"
-
-echo -e "\033[0;32mDeploying updates to Github...\033[0m"
-
-# Push source and build repos.
+git commit -m "rebuilding site"
 git push origin master
-
-echo -e "\033[0;32mChanges successfully published!\033[0m"
